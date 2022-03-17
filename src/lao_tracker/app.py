b17dabbf9154688f7f1a2737a316365767ec46a7
unknown <tom.eckstein@web.de>
2022-03-17 19:11:56 +0100

    Add functionality

--- src/lao_tracker/app.py
+++ src/lao_tracker/app.py
@@ -1,8 +1,9 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, State
 
 import cassiopeia as cass
 from cassiopeia.data import Queue, Position
-from cassiopeia.core import Summoner
+from cassiopeia.core import Summoner, Match
+import plotly.express as ex
 
 import lao_tracker.configure_cass
 
@@ -33,6 +34,25 @@ def get_text():
 
     return text
 
+def get_wards_table():
+    summoner = Summoner(name='xXLAOKOONXx', region='EUW')
+    match_history = cass.get_match_history(continent=summoner.region.continent,
+        puuid=summoner.puuid,
+        queue=Queue.ranked_solo_fives,)
+    last_ten_matches = match_history[0:10]
+    ward_count = []
+    ward_purchased = []
+    for match in last_ten_matches:
+        m: Match = match
+        p: cass.core.match.Participant = m.participants[summoner]
+        s: cass.core.match.ParticipantStats = p.stats
+        ward_count.append(s.wards_placed)
+        ward_purchased.append(s.vision_wards_bought)
+    data = {'Wards placed': ward_count, 'Wards purchased': ward_purchased}
+    return data
+
+
+
 app.layout = html.Div(children=[
     html.H1(children='Hello World!'),
 
@@ -45,7 +65,26 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Here will be even more content soon...
     '''),
+    html.Button('Refresh', id='refresh-val', n_clicks=0),
+    
+    html.Div(id='refresh-msg'),
+    dcc.Graph(id='wards-graph'),
 ])
+
+@app.callback(
+    Output('refresh-msg', 'children'),
+    Output('wards-graph', 'figure'),
+    Input('refresh-val', 'n_clicks'),
+)
+def refresh_data(n_clicks):
+    msg = f'Clicked {n_clicks} times.'
+
+    data = get_wards_table()
+
+    fig = ex.line(data, labels = 'Wards placed')
+
+    return msg, fig
+
 
 server = app.server
 
